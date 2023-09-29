e99f73adde873c5d423e440674c4c7ed0a1d9fd4
TanushaVeraksa <veraksa161@gmail.com>
2023-09-29 04:56:38 +0300

    changed url

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const $host = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://task4-b6a1enen1-tanushaveraksa.vercel.app/'
 })
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://task4-b6a1enen1-tanushaveraksa.vercel.app/'
 })
 
 const authInterceptor = config => {
